9a940bb94759f999c53a755c1615a4e3ebd65871
omkar <panageomkarnath@gmail.com>
2023-01-28 12:14:20 +0530

    sol

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,5 +1,25 @@
 package com.driver;
 
 public class Main {
+      public static class Product{
+          public static int product(int x , int y){
+              return x * y;
+          }
 
+          public static int product(int x , int y ,int z){
+              return x * y * z;
+          }
+          public static double product(double x , double y){
+              return x * y;
+          }
+       }
+
+    public static void main(String[] args) {
+        Product p = new Product();
+        p.product(1,2);
+        p.product(2.3 , 4.5);
+        p.product(1,2,3);
+
+
+    }
 }
